3d8e8a385c479d1eb4332c95d2c7af50f280aef5
Pichayanon Toochinda <kewkunggt@gmail.com>
2023-09-09 14:31:51 +0700

    Implement unit test for can_vote

--- polls/tests.py
+++ polls/tests.py
@@ -63,6 +63,42 @@ class QuestionModelTests(TestCase):
         past_question = Question(pub_date=time)
         self.assertIs(past_question.is_published(), True)
 
+    def test_can_vote_after_pub_date_before_end_date(self):
+        """
+        can_vote() returns True if the pub_data is in the past
+        and end_date is in the future.
+        """
+        past = timezone.now() - datetime.timedelta(days=30)
+        future = timezone.now() + datetime.timedelta(days=30)
+        question = Question(pub_date=past, end_date=future)
+        self.assertIs(question.can_vote(), True)
+
+    def test_can_vote_after_pub_date_null_end_date(self):
+        """
+        can_vote() returns True if the pub_data is in the past
+        and end_date is null.
+        """
+        past = timezone.now() - datetime.timedelta(days=30)
+        question = Question(pub_date=past)
+        self.assertIs(question.can_vote(), True)
+
+    def test_cannot_vote_before_pub_date(self):
+        """
+        can_vote() returns False if the pub_date is in the future.
+        """
+        future = timezone.now() + datetime.timedelta(days=30)
+        question = Question(pub_date=future)
+        self.assertIs(question.can_vote(), False)
+
+    def test_cannot_vote_after_end_date(self):
+        """
+        can_vote() returns False if the end_date is in the past.
+        """
+        past = timezone.now() - datetime.timedelta(days=30)
+        now = timezone.now()
+        question = Question(pub_date=now, end_date=past)
+        self.assertIs(question.can_vote(), False)
+
 
 def create_question(question_text, days):
     """
