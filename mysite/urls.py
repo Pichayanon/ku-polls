56fd0e80e4d3d41943a2b80c7dab2c73615ccb36
Pichayanon Toochinda <kewkunggt@gmail.com>
2023-09-10 13:26:58 +0700

    Update redirection

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='polls', permanent=True)),
-    path('polls/', include('polls.urls'), name='polls'),
-    path('admin/', admin.site.urls, name='admin'),
+    path('', RedirectView.as_view(pattern_name='polls:index', permanent=True)),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
