a6651e1e25f12255e2cc5da37a285ecaa4fdcbb5
Pichayanon Toochinda <kewkunggt@gmail.com>
2023-09-16 17:48:32 +0700

    Add sign up page for new users

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='polls:index', permanent=True)),
     path('polls/', include('polls.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup'),
     path('admin/', admin.site.urls),
 ]
